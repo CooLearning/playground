fa47dd35f3708dd85b860c450b0f2a293372b3e1
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-11-10 17:22:03 +0100

    fix(release): Improve `publish-and-deploy` performance + Correctly add the archive to released assets

--- release.config.js
+++ release.config.js
@@ -5,25 +5,28 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       ['@semantic-release/changelog', {
-        'changelogFile': 'CHANGELOG.md',
+        changelogFile: 'CHANGELOG.md',
       }],
       ['@semantic-release/exec', {
-        'prepareCmd': 'yarn build:archive ${nextRelease.version}',
+        prepareCmd: 'yarn build:archive ${nextRelease.version}',
       }],
       ['@semantic-release/npm', {
         npmPublish: false,
       }],
-      '@semantic-release/github',
+      ['@semantic-release/github', {
+        assets: [
+          'coolearning-playground-v${nextRelease.version}.zip',
+        ],
+      }],
       ['@qiwi/semantic-release-gh-pages-plugin', {
         src: 'dist',
       }],
       ['@semantic-release/git', {
-        'assets': [
+        assets: [
           'CHANGELOG.md',
           'package.json',
-          'coolearning-playground-v${nextRelease.version}.zip',
         ],
-        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       }],
     ],
 };
